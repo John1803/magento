302959ed5057ff1f24c0620e5bfd7501db08fe3c
Ivan Udovyk <ivanud@default-value.com>
2015-01-26 11:48:56 +0200

    simple model

--- app/code/local/Olelookoie/Weblog/controllers/IndexController.php
+++ app/code/local/Olelookoie/Weblog/controllers/IndexController.php
@@ -4,7 +4,46 @@ class Olelookoie_Weblog_IndexController extends Mage_Core_Controller_Front_Actio
 {
     public function testModelAction()
     {
+        $params = $this->getRequest()->getParams();
         $blogPost = Mage::getModel('weblog/blogpost');
-        echo get_class($blogPost);
+        echo("Loading the blogpost with an ID of ".$params['id']);
+        $blogPost->load($params['id']);
+        $data = $blogPost->getData();
+        var_dump($data);
+    }
+
+    public function createNewPostAction()
+    {
+        $blogPost = Mage::getModel('weblog/blogpost');
+        $blogPost->setTitle('Code Post!');
+        $blogPost->setPost('This post was created from code!');
+        $blogPost->save();
+        echo 'post with ID ' . $blogPost->getId() . ' created';
+    }
+
+    public function editFirstPostAction()
+    {
+        $blogPost = Mage::getModel('weblog/blogpost');
+        $blogPost->load(1);
+        $blogPost->setTitle('The 1st post!');
+        $blogPost->save();
+        echo 'post edited';
+    }
+
+    public function deleteFirstPostAction()
+    {
+        $blogPost = Mage::getModel('weblog/blogpost');
+        $blogPost->load(1);
+        $blogPost->delete();
+        echo 'post removed';
+    }
+
+    public function showAllBlogPostsAction()
+    {
+        $posts = Mage::getModel('weblog/blogpost')->getCollection();
+        foreach ($posts as $blogPost) {
+            echo '<h3>'.$blogPost->getTitle().'</h3>';
+            echo nl2br($blogPost->getPost());
+        }
     }
 }
