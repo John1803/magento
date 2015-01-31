882aa93887da387f33124bf6e14d28e07aab6b7f
Ivan Udovyk <ivanud@default-value.com>
2015-01-31 11:19:18 +0200

    added Olelukoie_News_Helper_Image for images processing

--- app/code/community/Olelukoie/News/Helper/Image.php
+++ app/code/community/Olelukoie/News/Helper/Image.php
@@ -5,7 +5,7 @@ class Olelukoie_News_Helper_Image extends Mage_Core_Helper_Abstract
     /**
      * Media path to extension images
      *
-     *@vat string
+     * @vat string
      */
     const MEDIA_PATH = 'news';
 
@@ -50,11 +50,147 @@ class Olelukoie_News_Helper_Image extends Mage_Core_Helper_Abstract
      *
      * @var array
      */
-    protected  $_imageSize = [
+    protected $_imageSize = [
         'minheight' => self::MIN_HEIGHT,
         'maxheight' => self::MAX_HEIGHT,
         'minwidth' => self::MIN_WIDTH,
         'maxwidth' => self::MAX_WIDTH
     ];
 
+    /**
+     * Array of allowed file extensions
+     *
+     * @var array
+     */
+    protected $_allowedExtensions = ['jpg', 'gif', 'png'];
+
+    /**
+     * Return the base media directory for News Item images
+     *
+     * @return string
+     */
+    public function getBaseDir()
+    {
+        return Mage::getBaseDir('media') . DS . self::MEDIA_PATH;
+    }
+
+    /**
+     * Return the Base URL for News Item images
+     *
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return Mage::getBaseUrl('media') . DS . self::MEDIA_PATH;
+    }
+
+    /**
+     * Remove news item image by image filename
+     *
+     * @param string $imageFile
+     * @return bool
+     */
+    public function removeImage($imageFile)
+    {
+        $io = new Varien_Io_File();
+        $io->open(['path' => $this->getBaseDir()]);
+        if ($io->fileExists($imageFile)) {
+            return $io->rm($imageFile);
+        }
+
+        return false;
+    }
+
+    /**
+     * Upload image and return uploaded image file name or false
+     *
+     * @throws Mage_Core_Exception
+     * @param string $scope the request key for file
+     * @return bool|string
+     */
+    public function uploadImage($scope)
+    {
+        $adapter = new Zend_File_Transfer_Adapter_Http();
+        $adapter->addValidator('ImageSize', true, $this->_imageSize);
+        $adapter->addValidator('Size', true, self::MAX_FILE_SIZE);
+        if ($adapter->isUploaded($scope)) {
+            //validate image
+            if (!$adapter->isValid($scope)) {
+                Mage::throwException(Mage::helper('olelukoie_newss')->__('Uploded image is not valid'));
+            }
+        }
+        $upload = new Varien_File_Uploader($scope);
+        $upload
+            ->setAllowCreateFolders(true)
+            ->setAllowedExtensions($this->_allowedExtensions)
+            ->setAllowRenameFiles(true)
+            ->setFilesDispersion(false);
+        if ($upload->save($this->getBaseDir())) {
+            return $upload->getUploadedFileName();
+        }
+        return false;
+    }
+
+    /**
+     * Return URL for resized News Item Image
+     *
+     * @param Olelukoie_News_Model_News $item
+     * @param integer $width
+     * @param integer $height
+     * @return bool|string
+     */
+    public function resize(Olelukoie_News_Model_News $item, $width, $height = null)
+    {
+        if ($item->getImage()) {
+            return false;
+        }
+
+        if ($width < self::MIN_WIDTH || $width > self::MAX_WIDTH) {
+            return false;
+        }
+        $width = (int)$width;
+
+        if (!is_null($height)) {
+            if ($height < self::MIN_HEIGHT || $height > self::MAX_HEIGHT) {
+                return false;
+            }
+            $height = int($height);
+        }
+
+        $imageFile = $item->getImage();
+        $cacheDir = $this->getBaseDir() . DS . 'csche' . DS . $width;
+        $cacheUrl = $this->getBaseUrl() . DS . 'cache' . DS . $width . DS;
+
+        $io = new Varien_Io_File();
+        $io->checkAndCreateFolder($cacheDir);
+        $io->open(['path' => $cacheDir]);
+        if ($io->fileExists($imageFile)) {
+            return $cacheUrl . $imageFile;
+        }
+
+        try {
+            $image = new Varien_Image($this->getBaseDir() . DS . $imageFile);
+            $image->resize($width, $height);
+            $image->save($cacheDir . DS . $imageFile);
+            return $cacheUrl . $imageFile;
+        } catch (Exception $e) {
+            Mage::logException($e);
+            return false;
+        }
+    }
+
+    /**
+     * Removes folder with cached images
+     *
+     * $return boolean
+     */
+    public function flushImagesCache()
+    {
+        $cacheDir = $this->getBaseDir() . DS . 'cache' . DS;
+        $io = new Varien_Io_File();
+        if ($io->fileExists($cacheDir, false)) {
+            return $io->rmdir($cacheDir, true);
+        }
+        return true;
+    }
 }
